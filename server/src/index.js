c56b4404b2c12fd6a9f598b0d200fa856010a620
hmassonn <hmassonn@42.student.fr>
2024-05-24 14:14:57 +0200

    get the station with lower price in 2km

--- server/src/index.js
+++ server/src/index.js
@@ -5,12 +5,16 @@ const cors = require('cors');
 const downloadZip = require('./downloadZip');
 const unzipFile = require('./unzipFile');
 const readXml = require('./readXml');
+const { all } = require("axios");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(cors()); // Ajoute le middleware CORS à toutes les routes
+
+const DISTANCE_MAX = 2; // in km
+const FUEL_TYPE = 'Gazole';
 
 app.get("/api", async (req, res) => {
   try {
@@ -33,12 +37,57 @@ app.get("/search", async (req, res) => {
     }
 });
 
+function toRadians(degrees) {
+    return degrees * (Math.PI / 180);
+}
+  
+function haversineDistance(coord1, coord2) {
+    const R = 6371; // Rayon de la Terre en kilomètres
+
+    const lat1 = toRadians(coord1.y);
+    const lon1 = toRadians(coord1.x);
+    const lat2 = toRadians(coord2.y);
+    const lon2 = toRadians(coord2.x);
+  
+    const dLat = lat2 - lat1;
+    const dLon = lon2 - lon1;
+  
+    const a = Math.sin(dLat / 2) ** 2 +
+              Math.cos(lat1) * Math.cos(lat2) *
+              Math.sin(dLon / 2) ** 2;
+  
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  
+    const distance = R * c;
+    return distance;
+}
+
 app.get("/find", async (req, res) => {
+    let all_stations = []
+    let price_min = -1;
+    let winner_station = {}
     try {
         downloadZip().then((token) => {
             setTimeout(()=>{
                 unzipFile(token).then(()=>{
-                    readXml(token);
+                    readXml(token).then((xml) => {
+                        if (xml && xml.pdv_liste && xml.pdv_liste.pdv) {
+                            all_stations = xml.pdv_liste.pdv.filter((element) => 
+                                haversineDistance({x: req.query.x, y: req.query.y}, {x: element.$.longitude / 100000, y: element.$.latitude / 100000}) < DISTANCE_MAX
+                            )
+                            all_stations.forEach(station => {
+                                if (station.prix) {
+                                    station.prix.forEach((price) => {
+                                        if (price_min == -1 || (price.$.nom == FUEL_TYPE && price.$.valeur < price_min)) {
+                                            price_min = price.$.valeur;
+                                            winner_station = station;
+                                        }
+                                    })
+                                }
+                            });
+                            res.json(winner_station)
+                        }
+                    });
                 });
             }, 1000);
         });
--- server/src/readXml.js
+++ server/src/readXml.js
@@ -3,27 +3,30 @@ const path = require('path');
 const xml2js = require('xml2js');
 
 // Fonction pour lire et analyser le fichier XML
-async function readXml(file_path) {
-  try {
-    const xmlFilePath = path.resolve(__dirname+"/uncompress_files", file_path+'/PrixCarburants_instantane.xml');
-    // Lire le contenu du fichier XML
-    const xmlData = fs.readFileSync(xmlFilePath, 'utf-8');
+function readXml(file_path) {
+  return new Promise((resolve, reject) => {
+    try {
+      const xmlFilePath = path.resolve(__dirname+"/uncompress_files", file_path+'/PrixCarburants_instantane.xml');
+      // Lire le contenu du fichier XML
+      const xmlData = fs.readFileSync(xmlFilePath, 'utf-8');
 
-    // Créer un parseur XML
-    const parser = new xml2js.Parser();
+      // Créer un parseur XML
+      const parser = new xml2js.Parser();
 
-    // Analyser le fichier XML en un objet JavaScript
-    parser.parseString(xmlData, (err, result) => {
-      if (err) {
-        throw err;
-      }
-
-      // Afficher l'objet JavaScript résultant
-      console.log('Objet JavaScript:', JSON.stringify(result, null, 2));
-    });
-  } catch (error) {
-    console.error('Erreur lors de la lecture ou de l\'analyse du fichier XML:', error);
-  }
+      // Analyser le fichier XML en un objet JavaScript
+      parser.parseString(xmlData, (err, result) => {
+        if (err) {
+          throw err;
+        }
+        // Afficher l'objet JavaScript résultant
+        // console.log(result.pdv_liste.pdv[0]);
+        resolve(result);
+      });
+    } catch (error) {
+      console.error('Erreur lors de la lecture ou de l\'analyse du fichier XML:', error);
+      reject()
+    }
+  })
 }
 
 module.exports = readXml;
