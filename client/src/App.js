bd58984779e62d8f1234a2580c726092bba4bc95
hmassonn <hmassonn@42.student.fr>
2024-05-24 15:13:32 +0200

    un peu de clean

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,6 @@ import Map from './components/map.js';
 import "./App.css";
 
 function App() {
-  // const [data, setData] = React.useState(null);
-
-  // React.useEffect(() => {
-  //   fetch("/api")
-  //     .then((res) => res.json())
-  //     .then((data) => setData(data.message));
-  // }, []);
-
   return (
     <div className="App">
       <Navbar/>
